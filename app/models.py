c1b14fa12fe57d31467f6754572472cf0dd8f104
Ashutosh Srivastav <developerashutoshsrivastav@gmail.com>
2026-02-21 22:36:03 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,14 +1,24 @@
 from pydantic import BaseModel
-from typing import Union
+from typing import List, Union
 
 class Message(BaseModel):
     sender: str
     text: str
-    timestamp: Union[str, int]
+    timestamp: Union[str, int, float]
+
+class Metadata(BaseModel):
+    channel: str
+    language: str
+    locale: str
+
+class ConversationItem(BaseModel):
+    sender: str
+    text: str
+    timestamp: Union[str, int, float]
 
 class HoneypotRequest(BaseModel):
     sessionId: str
     message: Message
-    conversationHistory: List[Message] = []
-
-    metadata: Dict[str, str]
+    conversationHistory: List[ConversationItem]
+    metadata: Metadata
+HoneypotRequest.model_rebuild()
